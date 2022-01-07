5dc06516ed0c21f5a54da042afb01ec3742f85ea
~Ryosuke <~ryosuke.810.109@gmail.com~>
2022-01-07 05:53:11 +0000

    update

--- myled/myled.c
+++ myled/myled.c
@@ -9,7 +9,6 @@
 #include <linux/device.h>
 #include <linux/uaccess.h>
 #include <linux/io.h>
-#include <linux/delay.h>
 
 #define num 3
 
@@ -28,68 +27,17 @@ static int list[num] = {25,26,12};
 static ssize_t led_write(struct file* flip, const char*buf, size_t count, loff_t*pos)
 {
 	char c;
-	int i,n;
 	if(copy_from_user(&c,buf,sizeof(char)))
 		return -EFAULT;
 
-	if(c && c != "\n"){
-
-	if(c == "a"){
-		for(i = 0; i <= 10; i++){
-			n = 1000 - 100*i + 1;
-			gpio_base[7] = 1 << list[0];
-			gpio_base[7] = 1 << list[2];
-			msleep(10);
-			gpio_base[10] = 1 << list[2];
-			msleep(n);
-
-			gpio_base[10] = 1 << list[0];
-			gpio_base[7] = 1 << list[1];
-			gpio_base[7] = 1 << list[2];
-			msleep(10);
-			gpio_base[10] = 1 << list[2];
-			msleep(n);
-			gpio_base[10] = 1 << list[1];
-
-			if(n == 1){
-				for(i = 0; i <= 5; i++){
-					gpio_base[7] = 1 << list[0];
-					gpio_base[7] = 1 << list[2];
-					msleep(10);
-					gpio_base[10] = 1 << list[2];
-					msleep(n);
-
-					gpio_base[10] = 1 << list[0];
-					gpio_base[7] = 1 << list[1];
-					gpio_base[7] = 1 << list[2];
-					msleep(10);
-					gpio_base[10] = 1 << list[2];
-					msleep(n);
-					gpio_base[10] = 1 << list[1];
-				}
-				break;
-			}
-		}
-			gpio_base[7] = 1 << list[2];
-			msleep(500);
-			gpio_base[10] = 1 << list[2];
-	}else{
-		for(i = 0; i <= 2; i++){
-			gpio_base[7] = 1 << list[0];
-			gpio_base[7] = 1 << list[1];
-			gpio_base[7] = 1 << list[2];
-			msleep(100);
-			gpio_base[10] = 1 << list[0];
-			gpio_base[10] = 1 << list[1];
-			gpio_base[10] = 1 << list[2];
-			msleep(100);
-		}
-	}
-}
-	
-	printk(KERN_INFO "receive: %c\n",c);
-	for(i = 0; i < num; i++){
-		gpio_base[10] = 1 << list[i];
+	if(c == '0'){
+		gpio_base[10] = 1 << list[0];
+		gpio_base[10] = 1 << list[1];
+		gpio_base[10] = 1 << list[2];
+	}else if(c == '1'){
+		gpio_base[7] = 1 << list[0];
+		gpio_base[7] = 1 << list[1];
+		gpio_base[7] = 1 << list[2];
 	}
 
 	return 1;
